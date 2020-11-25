b3cd9f6f9cf1d09e3d5a9b8879752c039688e6bf
Alberto Faria <albertofaria.dev@gmail.com>
2020-11-25 22:32:42 +0000

    Revamp token, loan, and market contracts

---
+++ contracts/TuiChainLoan.sol
@@ -0,0 +1,425 @@
+// SPDX-License-Identifier: UNLICENSED
+/* -------------------------------------------------------------------------- */
+
+pragma solidity ^0.6.0;
+
+import "./TuiChainToken.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+/* -------------------------------------------------------------------------- */
+
+contract TuiChainLoan is Ownable
+{
+    enum Phase { Funding, Expired, Canceled, Active, Finalized }
+
+    /**
+     * Emitted every time the phase is updated.
+     *
+     * @param newPhase The value to which the phase changed
+     */
+    event PhaseUpdated(Phase newPhase);
+
+    /**
+     * Emitted every time funds are provided.
+     *
+     * @param funder The address of the account or contract that provided the
+     *     funds
+     * @param providedFundsAttoDai The amount of funds provided (excluding
+     *     fees), in atto-Dai
+     * @param totalFundedAttoDai The new total amount of available funds
+     *     (excluding fees), in atto-Dai
+     */
+    event FundsProvided(
+        address funder, uint256 providedFundsAttoDai,
+        uint256 totalFundedAttoDai);
+
+    /**
+     * Emitted every time funds are withdrawn.
+     *
+     * @param funder The address of the account or contract that withdrew the
+     *     funds
+     * @param withdrawnFundsAttoDai The amount of funds withdrawn (excluding
+     *     fees), in atto-Dai
+     * @param totalFundedAttoDai The new total amount of available funds
+     *     (excluding fees), in atto-Dai
+     */
+    event FundsWithdrawn(
+        address funder, uint256 withdrawnFundsAttoDai,
+        uint256 totalFundedAttoDai);
+
+    /**
+     * Emitted every time tokens are claimed.
+     *
+     * @param claimer The address of the account or contract that claimed the
+     *     tokens
+     * @param amountTokens The number of tokens that were claimed
+     */
+    event TokensClaimed(address claimer, uint256 amountTokens);
+
+    /**
+     * Emitted every time a payment is made.
+     *
+     * @param payer The address of the account or contract that made the payment
+     * @param paymentAttoDai The payment's value (excluding fees), in atto-Dai
+     * @param totalPaidAttoDai The new total paid value (excluding fees), in
+     *     atto-Dai
+     */
+    event PaymentMade(
+        address payer, uint256 paymentAttoDai, uint256 totalPaidAttoDai);
+
+    /**
+     * Emitted every time tokens are redeemed.
+     *
+     * @param redeemer The address of the account or contract that redeemed the
+     *     tokens
+     * @param amountTokens The number of tokens that were redeemed
+     */
+    event TokensRedeemed(address redeemer, uint256 amountTokens);
+
+    /* ---------------------------------------------------------------------- */
+
+    using SafeERC20 for IERC20;
+    using SafeERC20 for TuiChainToken;
+    using SafeMath for uint256;
+
+    // The Dai contract.
+    IERC20 private immutable dai;
+
+    address private immutable feeRecipient;
+    address private immutable loanRecipient;
+    uint256 private immutable expirationTime;
+    uint256 private immutable fundingFeeAttoDaiPerDai;
+    uint256 private immutable paymentFeeAttoDaiPerDai;
+    uint256 private immutable requestedValueDai;
+
+    // The ERC-20 contract implementing the tokens for the loan. Only valid if
+    // phase is Active or Finalized.
+    TuiChainToken private immutable token;
+
+    // The current phase of the loan.
+    Phase private phase;
+
+    // How much Dai has been funded. This may increase and decrease while phase
+    // is Active, and equals requestedValueDai when phase is Active or
+    // Finalized.
+    uint256 private fundedDai;
+
+    // How many tokens each address that funded the loan can still claim.
+    mapping(address => uint256) private unclaimedTokens;
+
+    // How much Dai has been paid by the student (or anyone, really) so far.
+    uint256 private paidDai;
+
+    // How much atto-Dai a token can be redeemed for. This is zero is every
+    // phase but Finalized.
+    uint256 private attoDaiPerToken;
+
+    /* ---------------------------------------------------------------------- */
+
+    /**
+     * Note that the deployer is automatically assigned as the owner.
+     *
+     * @param _dai The Dai contract
+     * @param _feeRecipient Address of EOA or contract to which fees are to be
+     *     transferred
+     * @param _loanRecipient Address of EOA or contract to which the loan value
+     *     is to be transferred
+     * @param _secondsToExpiration Maximum amount of time in seconds for the
+     *     loan to be fully funded, after which it becomes expired
+     * @param _fundingFeeAttoDaiPerDai Funding fee, in atto-Dai per Dai
+     * @param _paymentFeeAttoDaiPerDai Payment fee, in atto-Dai per Dai
+     * @param _requestedValueAttoDai Total requested loan value, in atto-Dai
+     */
+    constructor(
+        IERC20 _dai,
+        address _feeRecipient,
+        address _loanRecipient,
+        uint256 _secondsToExpiration,
+        uint256 _fundingFeeAttoDaiPerDai,
+        uint256 _paymentFeeAttoDaiPerDai,
+        uint256 _requestedValueAttoDai
+        ) public
+    {
+        require(_dai != IERC20(0));
+        require(_feeRecipient != address(0));
+        require(_loanRecipient != address(0));
+        require(_secondsToExpiration > 0);
+
+        uint256 _requestedValueDai = _attoDaiToPositiveWholeDai(
+            _requestedValueAttoDai);
+
+        dai = _dai;
+
+        feeRecipient            = _feeRecipient;
+        loanRecipient           = _loanRecipient;
+        expirationTime          = block.timestamp.add(_secondsToExpiration);
+        fundingFeeAttoDaiPerDai = _fundingFeeAttoDaiPerDai;
+        paymentFeeAttoDaiPerDai = _paymentFeeAttoDaiPerDai;
+        requestedValueDai       = _requestedValueDai;
+
+        token = new TuiChainToken(this, _requestedValueDai);
+
+        phase           = Phase.Funding;
+        fundedDai       = 0;
+        paidDai         = 0;
+        attoDaiPerToken = 0;
+    }
+
+    /* ---------------------------------------------------------------------- */
+
+    /**
+     * Ensure that the given value, which is expected to be in atto-Dai, is
+     * positive and a multiple of 10^18, i.e., represents a positive and whole
+     * amount of Dai, and return the given value converted to Dai.
+     */
+    function _attoDaiToPositiveWholeDai(
+        uint256 _attoDai) private pure returns (uint256 _dai)
+    {
+        require(_attoDai > 0 && _attoDai.mod(1e18) == 0);
+
+        return _attoDai.div(1e18);
+    }
+
+    function _updatePhase(Phase _newPhase) private
+    {
+        assert(phase != _newPhase);
+
+        phase = _newPhase;
+        emit PhaseUpdated(_newPhase);
+    }
+
+    function _tryExpire() private
+    {
+        if (phase == Phase.Funding && block.timestamp >= expirationTime)
+            _updatePhase(Phase.Expired);
+    }
+
+    /* ---------------------------------------------------------------------- */
+
+    /**
+     * Cancel the loan.
+     *
+     * Only the owner can invoke this function.
+     *
+     * This also checks if the loan has expired beforehand.
+     *
+     * Fails if current phase is not Funding or if the invocation causes the
+     * contract to expire.
+     */
+    function cancel() external onlyOwner
+    {
+        _tryExpire();
+
+        // checks
+
+        require(phase == Phase.Funding);
+
+        // effects
+
+        _updatePhase(Phase.Canceled);
+    }
+
+    /**
+     * Finalize the loan.
+     *
+     * Phase must be Active and becomes Finalized.
+     */
+    function finalize() external onlyOwner
+    {
+        // checks
+
+        require(phase == Phase.Active);
+
+        // effects
+
+        _updatePhase(Phase.Finalized);
+
+        attoDaiPerToken = paidDai.mul(1e18).div(requestedValueDai);
+    }
+
+    /* ---------------------------------------------------------------------- */
+
+    /**
+     * Return the ERC-20 contract implementing the loan's tokens.
+     *
+     * This is always valid, but note that this loan contract always holds the
+     * entire token supply if Phase is Funding, Expired, or Canceled.
+     */
+    function getToken() public view returns (TuiChainToken _token)
+    {
+        return token;
+    }
+
+    /**
+     * Expire the loan if the funding deadline has passed.
+     *
+     * Fails if current phase is not Funding or Expired.
+     *
+     * @return _expired true if the loan becomes or already was expired, false
+     *     otherwise
+     */
+    function checkExpiration() public returns (bool _expired)
+    {
+        _tryExpire();
+
+        require(phase == Phase.Funding || phase == Phase.Expired);
+
+        return phase == Phase.Expired;
+    }
+
+    /**
+     * Deposit some Dai to fund the loan.
+     *
+     * Fees are added to _valueAttoDai. The actual transferred value, in
+     * atto-Dai, is given by:
+     *
+     *     _valueAttoDai + fundingFeeAttoDaiPerDai * (_valueAttoDai / 1e18)
+     *
+     * @param _valueAttoDai Amount of funds to provide (before fees) in atto-Dai
+     */
+    function provideFunds(uint256 _valueAttoDai) external
+    {
+        _tryExpire();
+
+        // checks
+
+        require(phase == Phase.Funding);
+
+        uint256 valueDai = _attoDaiToPositiveWholeDai(_valueAttoDai);
+        require(fundedDai.add(valueDai) <= requestedValueDai);
+
+        // effects
+
+        fundedDai = fundedDai.add(valueDai);
+        unclaimedTokens[msg.sender] = unclaimedTokens[msg.sender].add(valueDai);
+
+        emit FundsProvided(msg.sender, _valueAttoDai, fundedDai.mul(1e18));
+
+        if (fundedDai == requestedValueDai)
+            _updatePhase(Phase.Active);
+
+        // interactions
+
+        uint256 feeAttoDai = fundingFeeAttoDaiPerDai.mul(valueDai);
+
+        dai.safeTransferFrom(
+            msg.sender, address(this), _valueAttoDai.add(feeAttoDai));
+
+        if (fundedDai == requestedValueDai)
+        {
+            dai.safeTransfer(loanRecipient, requestedValueDai.mul(1e18));
+
+            dai.safeTransfer(
+                feeRecipient, fundingFeeAttoDaiPerDai.mul(requestedValueDai));
+        }
+    }
+
+    /**
+     * Withdraw some previously deposited Dai.
+     *
+     * Fees are added to _valueAttoDai. The actual transferred value, in
+     * atto-Dai, is given by:
+     *
+     *     _valueAttoDai + fundingFeeAttoDaiPerDai * (_valueAttoDai / 1e18)
+     *
+     * @param _valueAttoDai Amount of funds to withdraw (before fees) in
+     *     atto-Dai
+     */
+    function withdrawFunds(uint256 _valueAttoDai) external
+    {
+        _tryExpire();
+
+        // checks
+
+        require(
+            phase == Phase.Funding || phase == Phase.Expired
+            || phase == Phase.Canceled);
+
+        uint256 valueDai = _attoDaiToPositiveWholeDai(_valueAttoDai);
+        require(valueDai < unclaimedTokens[msg.sender]);
+
+        // effects
+
+        fundedDai = fundedDai.sub(valueDai);
+        unclaimedTokens[msg.sender] = unclaimedTokens[msg.sender].sub(valueDai);
+
+        emit FundsWithdrawn(msg.sender, _valueAttoDai, fundedDai.mul(1e18));
+
+        // interactions
+
+        uint256 feeAttoDai = fundingFeeAttoDaiPerDai.mul(valueDai);
+        dai.safeTransfer(msg.sender, _valueAttoDai.add(feeAttoDai));
+    }
+
+    function claimTokens(uint256 _amountTokens) external
+    {
+        // checks
+
+        require(phase == Phase.Active || phase == Phase.Finalized);
+        require(_amountTokens > 0);
+        require(_amountTokens <= unclaimedTokens[msg.sender]);
+
+        // effects
+
+        unclaimedTokens[msg.sender] =
+            unclaimedTokens[msg.sender].sub(_amountTokens);
+
+        emit TokensClaimed(msg.sender, _amountTokens);
+
+        // interactions
+
+        token.safeTransfer(msg.sender, _amountTokens);
+    }
+
+    /**
+     * TODO: document
+     */
+    function makePayment(uint256 _valueAttoDai) external
+    {
+        // checks
+
+        require(phase == Phase.Active);
+        uint256 valueDai = _attoDaiToPositiveWholeDai(_valueAttoDai);
+
+        // effects
+
+        paidDai = paidDai.add(valueDai);
+
+        emit PaymentMade(msg.sender, _valueAttoDai, paidDai.mul(1e18));
+
+        // interactions
+
+        uint256 feeAttoDai = paymentFeeAttoDaiPerDai.mul(valueDai);
+
+        dai.safeTransferFrom(
+            msg.sender, address(this), _valueAttoDai.add(feeAttoDai));
+
+        dai.safeTransfer(feeRecipient, feeAttoDai);
+    }
+
+    /**
+     * TODO: document
+     */
+    function redeemTokens(uint256 _amountTokens) external
+    {
+        // checks
+
+        require(phase == Phase.Finalized);
+        require(_amountTokens > 0);
+
+        // effects
+
+        emit TokensRedeemed(msg.sender, _amountTokens);
+
+        // interactions
+
+        token.burnFrom(msg.sender, _amountTokens);
+        dai.safeTransfer(msg.sender, attoDaiPerToken.mul(_amountTokens));
+    }
+}
+
+/* -------------------------------------------------------------------------- */
---
+++ contracts/TuiChainMarket.sol
@@ -0,0 +1,318 @@
+// SPDX-License-Identifier: UNLICENSED
+/* -------------------------------------------------------------------------- */
+
+pragma solidity ^0.6.0;
+
+import "./TuiChainToken.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+/* -------------------------------------------------------------------------- */
+
+contract TuiChainMarket is Ownable
+{
+    /**
+     * Emitted every time a purchase is made.
+     *
+     * @param token The ERC-20 contract implementing the token that was
+     *     transacted
+     * @param seller The address of the seller
+     * @param buyer The address of the buyer
+     * @param amountTokens The number of tokens that were transacted
+     * @param priceAttoDaiPerToken The value paid by the buyer to the seller for
+     *     each token (excluding fees), in atto-Dai
+     * @param feeAttoDai The total fee paid by the buyer, in atto-Dai
+     */
+    event Transaction(
+        TuiChainToken token, address seller, address buyer,
+        uint256 amountTokens, uint256 priceAttoDaiPerToken, uint256 feeAttoDai
+        );
+
+    /* ---------------------------------------------------------------------- */
+
+    struct SellPosition
+    {
+        uint256 amountTokens;
+        uint256 priceNanoDaiPerToken;
+    }
+
+    using SafeERC20 for IERC20;
+    using SafeERC20 for TuiChainToken;
+    using SafeMath for uint256;
+
+    // The Dai contract.
+    IERC20 private immutable dai;
+
+    // Address to which to send all collected fees.
+    address private immutable feeRecipient;
+
+    // Purchase fee, in atto-Dai per nano-Dai.
+    uint256 private feeAttoDaiPerNanoDai;
+
+    // True for an address if and only if it is the ERC-20 contract of a token
+    // allowed to be exchanged in the market.
+    mapping(TuiChainToken => bool) private allowedTokens;
+
+    // token address => seller address => sell position
+    mapping(TuiChainToken => mapping(address => SellPosition))
+        private sellPositions;
+
+    /* ---------------------------------------------------------------------- */
+
+    /**
+     * The owner can later change the fee with setFee().
+     *
+     * @param _dai The Dai contract
+     * @param _feeRecipient Address of EOA or contract to which fees are to be
+     *     transferred
+     * @param _feeAttoDaiPerNanoDai Purchase fee, in atto-Dai per nano-Dai
+     */
+    constructor(
+        IERC20 _dai,
+        address _feeRecipient,
+        uint256 _feeAttoDaiPerNanoDai
+        ) public
+    {
+        require(_dai != IERC20(0));
+        require(_feeRecipient != address(0));
+
+        dai = _dai;
+
+        feeRecipient         = _feeRecipient;
+        feeAttoDaiPerNanoDai = _feeAttoDaiPerNanoDai;
+    }
+
+    /* ---------------------------------------------------------------------- */
+
+    /**
+     * Ensure that the given value, which is expected to be in atto-Dai, is
+     * positive and a multiple of 10^9, i.e., represents a positive and whole
+     * amount of nano-Dai, and return the given value converted to nano-Dai.
+     */
+    function _attoDaiToPositiveWholeNanoDai(
+        uint256 _attoDai) private pure returns (uint256 _nanoDai)
+    {
+        require(_attoDai > 0 && _attoDai.mod(1e9) == 0);
+
+        return _attoDai.div(1e9);
+    }
+
+    /* ---------------------------------------------------------------------- */
+
+    // Add the given token to the set of allowed tokens.
+    function allowToken(TuiChainToken _token) external onlyOwner
+    {
+        require(_token != TuiChainToken(0));
+
+        allowedTokens[_token] = true;
+    }
+
+    // Set the fee to the given value.
+    function setFee(uint256 _feeAttoDaiPerNanoDai) external onlyOwner
+    {
+        feeAttoDaiPerNanoDai = _feeAttoDaiPerNanoDai;
+    }
+
+    /* ---------------------------------------------------------------------- */
+
+    /**
+     * Return the current fee, in atto-Dai per payed nano-Dai.
+     */
+    function getFee() external view returns (uint256 _feeAttoDaiPerNanoDai)
+    {
+        return feeAttoDaiPerNanoDai;
+    }
+
+    /**
+     * Add a new sell position.
+     *
+     * A sell position by the sender for the given token must not already exist.
+     */
+    function addSellPosition(
+        TuiChainToken _token, uint256 _amountTokens,
+        uint256 _priceAttoDaiPerToken) external
+    {
+        // checks
+
+        require(allowedTokens[_token]);
+
+        SellPosition storage position = sellPositions[_token][msg.sender];
+
+        require(position.amountTokens == 0, "sell position already exists");
+
+        require(_amountTokens > 0);
+
+        uint256 priceNanoDaiPerToken = _attoDaiToPositiveWholeNanoDai(
+            _priceAttoDaiPerToken);
+
+        // effects
+
+        position.amountTokens               = _amountTokens;
+        position.priceNanoDaiPerToken = priceNanoDaiPerToken;
+
+        // interactions
+
+        _token.safeTransferFrom(msg.sender, address(this), _amountTokens);
+    }
+
+    /**
+     * Remove an existing sell position.
+     */
+    function removeSellPosition(TuiChainToken _token) external
+    {
+        // checks
+
+        require(allowedTokens[_token]);
+
+        uint256 amountTokens = sellPositions[_token][msg.sender].amountTokens;
+
+        require(amountTokens > 0, "sell position does not exist");
+
+        // effects
+
+        delete sellPositions[_token][msg.sender];
+
+        // interactions
+
+        _token.safeTransfer(msg.sender, amountTokens);
+    }
+
+    /**
+     * Increase the token amount of an existing sell position.
+     *
+     * Functions are provided to increase and decrease the sell position amount
+     * instead of having a single function to set it to a given value in order
+     * to avoid attacks analogous to ERC-20's multiple withdrawal attack:
+     *
+     *     https://docs.google.com/document/d/1YLPtQxZu1UAvO9cZ1O2RPXBbT0mooh4DYKjA_jp-RLM
+     */
+    function increaseSellPositionAmount(
+        TuiChainToken _token, uint256 _increaseAmount) external
+    {
+        // checks
+
+        require(allowedTokens[_token]);
+        require(_increaseAmount > 0);
+
+        SellPosition storage position = sellPositions[_token][msg.sender];
+
+        require(position.amountTokens > 0, "sell position does not exist");
+
+        // effects
+
+        position.amountTokens = position.amountTokens.add(_increaseAmount);
+
+        // interactions
+
+        _token.safeTransferFrom(msg.sender, address(this), _increaseAmount);
+    }
+
+    /**
+     * Decrease the token amount of an existing sell position.
+     *
+     * If _decreaseAmount is the same as the sell position's amount, the sell
+     * position is removed.
+     *
+     * Functions are provided to increase and decrease the sell position amount
+     * instead of having a single function to set it to a given value in order
+     * to avoid attacks analogous to ERC-20's multiple withdrawal attack:
+     *
+     *     https://docs.google.com/document/d/1YLPtQxZu1UAvO9cZ1O2RPXBbT0mooh4DYKjA_jp-RLM
+     */
+    function decreaseSellPositionAmount(
+        TuiChainToken _token, uint256 _decreaseAmount) external
+    {
+        // checks
+
+        require(allowedTokens[_token]);
+        require(_decreaseAmount > 0);
+
+        SellPosition storage position = sellPositions[_token][msg.sender];
+
+        require(position.amountTokens > 0, "sell position does not exist");
+        require(_decreaseAmount <= position.amountTokens);
+
+        // effects
+
+        position.amountTokens = position.amountTokens.sub(_decreaseAmount);
+
+        if (position.amountTokens == 0)
+            position.priceNanoDaiPerToken = 0; // free up storage
+
+        // interactions
+
+        _token.safeTransfer(msg.sender, _decreaseAmount);
+    }
+
+    /**
+     * Update the price of an existing sell position.
+     */
+    function updateSellPositionPrice(
+        TuiChainToken _token, uint256 _newPriceAttoDaiPerToken) external
+    {
+        // checks
+
+        require(allowedTokens[_token]);
+
+        SellPosition storage position = sellPositions[_token][msg.sender];
+
+        require(position.amountTokens > 0, "sell position does not exist");
+
+        uint256 newPriceNanoDaiPerToken = _attoDaiToPositiveWholeNanoDai(
+            _newPriceAttoDaiPerToken);
+
+        // effects
+
+        position.priceNanoDaiPerToken = newPriceNanoDaiPerToken;
+    }
+
+    /**
+     * Purchase tokens offered by a sell position.
+     *
+     * The amount and price must be specified to ensure that they are what the
+     * user expects, since the sell position can be altered at any time.
+     */
+    function buy(
+        TuiChainToken _token, address _seller,
+        uint256 _amountTokens, uint256 _priceAttoDaiPerToken) external
+    {
+        // checks
+
+        require(allowedTokens[_token]);
+
+        SellPosition storage position = sellPositions[_token][_seller];
+
+        require(position.amountTokens > 0, "sell position does not exist");
+
+        uint256 priceNanoDaiPerToken = _attoDaiToPositiveWholeNanoDai(
+            _priceAttoDaiPerToken);
+
+        require(position.amountTokens == _amountTokens);
+        require(position.priceNanoDaiPerToken == priceNanoDaiPerToken);
+
+        // effects
+
+        delete sellPositions[_token][_seller];
+
+        uint256 priceAttoDai = _priceAttoDaiPerToken.mul(_amountTokens);
+        uint256 feeAttoDai   = feeAttoDaiPerNanoDai.mul(priceAttoDai.div(1e9));
+
+        emit Transaction(
+            _token, _seller, msg.sender,
+            _amountTokens, _priceAttoDaiPerToken, feeAttoDai
+            );
+
+        // interactions
+
+        dai.safeTransferFrom(msg.sender, _seller, priceAttoDai);
+        dai.safeTransferFrom(msg.sender, feeRecipient, feeAttoDai);
+
+        _token.safeTransfer(msg.sender, _amountTokens);
+
+    }
+}
+
+/* -------------------------------------------------------------------------- */
