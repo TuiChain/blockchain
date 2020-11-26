2c2d96a49116dd48eafb09868c42ec1575fe399f
Alberto Faria <albertofaria.dev@gmail.com>
2020-11-26 22:00:28 +0000

    Revamp token, loan, and market contracts (#25)

---
+++ contracts/TuiChainController.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: UNLICENSED
+/* -------------------------------------------------------------------------- */
+
+pragma solidity ^0.6.0;
+
+import "./TuiChainLoan.sol";
+import "./TuiChainMarket.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+/* -------------------------------------------------------------------------- */
+
+contract TuiChainController is Ownable
+{
+    // The Dai contract.
+    IERC20 private immutable dai;
+
+    TuiChainMarket private immutable market;
+
+    mapping(TuiChainLoan => bool) private loans;
+
+    /* ---------------------------------------------------------------------- */
+
+    /**
+     * The owner can later change the fee with setFee().
+     *
+     * @param _dai The Dai contract
+     * @param _marketFeeRecipient Address of EOA or contract to which market
+     *     fees are to be transferred
+     * @param _marketFeeAttoDaiPerNanoDai Market purchase fee, in atto-Dai per
+     *     nano-Dai
+     */
+    constructor(
+        IERC20 _dai,
+        address _marketFeeRecipient,
+        uint256 _marketFeeAttoDaiPerNanoDai
+        ) public
+    {
+        require(_dai != IERC20(0));
+
+        dai = _dai;
+
+        market = new TuiChainMarket(
+            _dai, _marketFeeRecipient, _marketFeeAttoDaiPerNanoDai);
+    }
+
+    /* ---------------------------------------------------------------------- */
+
+    /**
+     * TODO: document
+     */
+    function setMarketFee(
+        uint256 _marketFeeAttoDaiPerNanoDai) external onlyOwner
+    {
+        market.setFee(_marketFeeAttoDaiPerNanoDai);
+    }
+
+    /**
+     * TODO: document
+     */
+    function createLoan(
+        address _feeRecipient,
+        address _loanRecipient,
+        uint256 _secondsToExpiration,
+        uint256 _fundingFeeAttoDaiPerDai,
+        uint256 _paymentFeeAttoDaiPerDai,
+        uint256 _requestedValueAttoDai
+        ) external onlyOwner returns (TuiChainLoan _loan)
+    {
+        TuiChainLoan loan = new TuiChainLoan(
+            dai,
+            _feeRecipient,
+            _loanRecipient,
+            _secondsToExpiration,
+            _fundingFeeAttoDaiPerDai,
+            _paymentFeeAttoDaiPerDai,
+            _requestedValueAttoDai
+            );
+
+        market.allowToken(loan.getToken());
+
+        loans[loan] = true;
+
+        return loan;
+    }
+
+    /**
+     * TODO: document
+     */
+    function cancelLoan(TuiChainLoan _loan) external onlyOwner
+    {
+        require(loans[_loan]);
+
+        _loan.cancel();
+    }
+
+    /**
+     * TODO: document
+     */
+    function finalizeLoan(TuiChainLoan _loan) external onlyOwner
+    {
+        require(loans[_loan]);
+
+        _loan.finalize();
+    }
+
+    /* ---------------------------------------------------------------------- */
+
+    /**
+     * TODO: document
+     */
+    function getMarket() external view returns (TuiChainMarket _market)
+    {
+        return market;
+    }
+}
+
+/* -------------------------------------------------------------------------- */
---
+++ contracts/TuiChainToken.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+/* -------------------------------------------------------------------------- */
+
+pragma solidity ^0.6.0;
+
+import "./TuiChainLoan.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+
+/* -------------------------------------------------------------------------- */
+
+contract TuiChainToken is ERC20Burnable
+{
+    TuiChainLoan private immutable loan;
+
+    /* ---------------------------------------------------------------------- */
+
+    /**
+     * The entire supply is assigned to _loan.
+     */
+    constructor(TuiChainLoan _loan, uint256 _totalSupply) ERC20("", "") public
+    {
+        require(_loan != TuiChainLoan(0));
+
+        loan = _loan;
+
+        // TODO: document
+        _setupDecimals(0);
+
+        // assign entire supply to loan contract
+        _mint(address(_loan), _totalSupply);
+    }
+
+    /* ---------------------------------------------------------------------- */
+
+    /**
+     * Return the loan contract to which this token contract pertains.
+     */
+    function getLoan() external view returns (TuiChainLoan _loan)
+    {
+        return loan;
+    }
+}
+
+/* -------------------------------------------------------------------------- */
