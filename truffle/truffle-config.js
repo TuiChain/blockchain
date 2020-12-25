5dfe8843622476fe4441fcb50c9876819a293853
Alberto Faria <albertofaria.dev@gmail.com>
2020-12-25 06:48:45 +0000

    Change JavaScript indentation to 2 spaces

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -1,24 +1,24 @@
 /* -------------------------------------------------------------------------- */
 
 module.exports = {
-    networks: {
-        ganache: {
-            host: "localhost",
-            port: 7545,
-            network_id: "*",
-        },
+  networks: {
+    ganache: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*",
     },
-    compilers: {
-        solc: {
-            version: "0.6.12",
-            settings: {
-                optimizer: {
-                    enabled: true,
-                    runs: 200,
-                },
-            },
+  },
+  compilers: {
+    solc: {
+      version: "0.6.12",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
         },
+      },
     },
+  },
 };
 
 /* -------------------------------------------------------------------------- */
